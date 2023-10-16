ad5856441b21c69759e1ca14735d828e6752418e
Airan Telles <airan.telles@bravosul.com.br>
2023-10-16 01:48:18 -0300

    Create seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+invoice_types = InvoiceType.create(
+  [
+    { name: "Cartão de crédito"}, 
+    { name: "Boleto"}, 
+    { name: "Pix"}, 
+    { name: "Débito Automático"}
+  ]
+)
+
+Customer.create(
+  [
+    {name: "Usuário 1", day: 13, invoice_type_id: invoice_types[0].id},
+    {name: "Usuário 2", day: 17, invoice_type_id: invoice_types[1].id},
+    {name: "Usuário 3", day: 16, invoice_type_id: invoice_types[2].id},
+    {name: "Usuário 4", day: 19, invoice_type_id: invoice_types[3].id},
+    {name: "Usuário 5", day: 20, invoice_type_id: invoice_types[0].id},
+    {name: "Usuário 6", day: 25, invoice_type_id: invoice_types[2].id}
+  ]
+)
