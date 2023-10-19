27bd049e43c447f6d2fd085453f17384b08cc4b7
Airan Telles <airan.telles@bravosul.com.br>
2023-10-18 22:30:20 -0300

    Create invoices views and fix tests requests invoices.

--- spec/requests/invoices_spec.rb
+++ spec/requests/invoices_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Invoices", type: :request do
+  let(:customer) { create(:customer) }
+  let(:invoice_type) { create(:invoice_type) }
+  let(:invoice) { create(:invoice) }
+
   describe '#index' do
     it "should render index page" do
       get '/invoices'
       expect(response).to render_template :index
     end
     it 'assigns invoices' do
-      new_invoice = FactoryBot.create :invoice
+      FactoryBot.create :invoice
       get '/invoices'
-      expect(Invoice.first).to eq(new_invoice)
+      expect(assigns(:invoices)).to eq(Invoice.all)
     end
   end
 end
