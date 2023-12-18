fb6534af12ea64b95baaac5e1fe5c826d1cf9a90
Airan Telles <airan.telles@bravosul.com.br>
2023-12-18 13:43:53 -0300

    Config enveronment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.force_ssl = false
+  config.hosts << "ec2-18-226-251-145.us-east-2.compute.amazonaws.com"
 end
